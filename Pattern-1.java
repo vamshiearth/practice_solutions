4d1af6447e26560a50b21116f030944f85acd031
Vamshi <kasarla.vamshi@amadeus.com>
2023-03-19 11:25:45 +0530

    Added solutions

---
+++ Pattern-1.java
@@ -0,0 +1,12 @@
+
+class Solution {
+
+    void printSquare(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
